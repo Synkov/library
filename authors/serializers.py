7b1594633c2ec2aa89faee9413639c90a996fadc
aleksei <synkovaleksei@yandex.ru>
2021-12-27 00:08:56 +0300

    Lesson 3. Code style fix

--- authors/serializers.py
+++ authors/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
+
 from .models import Author
+
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
--- usersapp/models.py
+++ usersapp/models.py
@@ -1,9 +1,9 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid4())
     email = models.EmailField(unique=True, blank=False)
-
