f39ac87f7bf7bd7b04468245e54d1d669fdc2f7d
aleksei <synkovaleksei@yandex.ru>
2021-12-27 00:06:54 +0300

    Added usersapp. API done

--- library/settings.py
+++ library/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+AUTH_USER_MODEL = "usersapp.User"
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authors',
+    'usersapp',
 ]
 
 MIDDLEWARE = [
