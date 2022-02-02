b8af0c517842375c77a60c028aa1c6ee6cae27c4
aleksei <synkovaleksei@yandex.ru>
2022-02-03 00:07:25 +0300

    Lesson_8. Тестирование API.

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,3 +1,18 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import AuthorViewSet
+from .models import Author, Book
 
-# Create your tests here.
+
+class TestAuthorViewSet(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/authors/')
+        view = AuthorViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
