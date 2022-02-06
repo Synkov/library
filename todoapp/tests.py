4d7531abd4c6d80ada4c4da867c5abfa2c360eed
Synkov Aleksei <81877147+Synkov@users.noreply.github.com>
2022-02-06 19:45:28 +0300

    Lesson 8 (#8)
    * Lesson 3. Serializers. Renderers. Routers
    
    * Lesson 3. Code style fix
    
    * Lesson 4. Views. Filtering. Pagination. Code style fix
    
    * Lesson 5. Code style fix
    
    * Lesson 5. Routing. SPA
    
    * Lesson 6. Code style fix
    
    * Lesson 7. Code style fix
    
    * Lesson 7. Code style fix
    
    * Lesson_8. Тестирование API.
    
    * Lesson_8

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,3 +1,113 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from .views import ProjectModelViewSet, ToDoModelViewSet
+from .models import ToDo, Project
+from ..usersapp.models import User
 
-# Create your tests here.
+
+class TestProjectModelViewSet(TestCase):
+    def test_get_project_list(self):
+        factory = APIRequestFactory()
+        request = factory.get("/api/projects/")
+        view = ProjectModelViewSet.as_view({"get": "list"})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_ToDo_list(self):
+        factory = APIRequestFactory()
+        request = factory.get("/api/todo/")
+        view = ToDoModelViewSet.as_view({"get": "list"})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_project(self):
+        factory = APIRequestFactory()
+        data = {"name": "Test_project", "repository": "http://localhost", "users": []}
+        request = factory.post("/api/projects/", data=data, format="json")
+        view = ProjectModelViewSet.as_view({"post": "create"})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_project_with_auth(self):
+        factory = APIRequestFactory()
+
+        admin = User.objects.create_superuser("admin", "admin@local.host", "password")
+        admin_id = admin.id
+
+        data = {
+            "name": "Test_project",
+            "repository": "http://localhost",
+            "users": [admin_id],
+        }
+        request = factory.post("/api/projects/", data=data, format="json")
+
+        force_authenticate(request, admin)
+        view = ProjectModelViewSet.as_view({"post": "create"})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class TestToDoViewSet(APITestCase):
+    def test_get_ToDo_list(self):
+        response = self.client.get("/api/todo/")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        admin = User.objects.create_superuser("admin", "admin@local.host", "password")
+        self.client.login(username="admin", password="password")
+        response = self.client.get("/api/todo/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_edit_ToDo(self):
+        user = User.objects.create(
+            email="user@local.host", username="user", password="password"
+        )
+
+        project = Project.objects.create(
+            name="Test_project", repository="http://localhost"
+        )
+        project.users.add(user)
+
+        todo = ToDo.objects.create(
+            project=project, create=user, text="test text"
+        )
+
+        admin = User.objects.create_superuser("admin", "admin@local.host", "password")
+        self.client.login(username="admin", password="password")
+
+        response = self.client.put(
+            f"/api/todo/{todo.id}/",
+            {"project": project.id, "create": user.id, "text": "SOME TEXT"},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        todo = ToDo.objects.get(uuid=todo.uuid)
+        self.assertEqual(todo.task_text, "NEW TEXT")
+
+        self.client.logout
+
+    def test_edit_ToDo_mixer(self):
+
+        todo = mixer.blend(ToDo)
+
+        admin = User.objects.create_superuser("admin", "admin@local.host", "password")
+        self.client.login(username="admin", password="password")
+
+        response = self.client.put(
+            f"/api/todo/{todo.id}/",
+            {
+                "project": todo.project.id,
+                "create": todo.create.id,
+                "text": "SOME TEXT",
+            },
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        todo = ToDo.objects.get(uuid=todo.uuid)
+        self.assertEqual(todo.text, "SOME TEXT")
+
+        self.client.logout
--- todoapp/views.py
+++ todoapp/views.py
@@ -10,7 +10,7 @@ from rest_framework.viewsets import ModelViewSet
 
 >>>>>>> 17d7a11250244cf089fec8091b133655c22d6e89
 from .models import Project, ToDo
-from .serializers import ProjectModelSerializer, ToDoModelSerializer
+from .serializers import ProjectModelSerializer, ToDoModelSerializer, ToDoModelSerializerBase
 
 
 <<<<<<< HEAD
@@ -40,6 +40,11 @@ class ToDoModelViewSet(ModelViewSet):
 <<<<<<< HEAD
     pagination_class = ToDoPagination
     filterset_class = ToDoFilter
+
+    def get_serializer_class(self):
+        if self.request.method in ["GET"]:
+            return ToDoModelSerializer
+        return ToDoModelSerializerBase
 
     def destroy(self, request, *args, **kwargs):
         todo = self.get_object()
