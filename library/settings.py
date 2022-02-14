31d705ef17cb796c8415aa209365fc1f15571082
aleksei <synkovaleksei@yandex.ru>
2022-02-14 13:44:24 +0300

    urls + settings

--- library/settings.py
+++ library/settings.py
@@ -44,10 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-<<<<<<< HEAD
     "graphene_django",
-=======
->>>>>>> master
     "drf_yasg",
     "rest_framework",
     "authors",
--- library/urls.py
+++ library/urls.py
@@ -2,20 +2,14 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-<<<<<<< HEAD
 from graphene_django.views import GraphQLView
-=======
->>>>>>> master
 from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-
 from todoapp.views import ProjectModelViewSet, ToDoModelViewSet
-<<<<<<< HEAD
 from usersapp.views import UserViewSet
-=======
 from usersapp.views import UserViewSet, UserViewSet
->>>>>>> master
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -39,17 +33,8 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(router.urls)),
     path("api-token-auth/", views.obtain_auth_token),
-<<<<<<< HEAD
-    # path("api/v<int:version>/users/", UserViewSet.as_view({"get": "list"})),
     path("api/v<str:version>/", include(router.urls)),
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("graphql/", GraphQLView.as_view(graphiql=True), name="graphql"),
-=======
-    path("api/v<int:version>/users/", UserViewSet.as_view({"get": "list"})),
-    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
->>>>>>> master
 ]
